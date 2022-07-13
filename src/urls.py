c1e26137f6cf6490e56a40aad93ff56d19a1e6ce
unknown <gabrielthe13@hotmail.com>
2022-07-13 02:16:48 -0300

    start formats

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,32 @@
+import pymongo
 from flask import Flask
+
+from src.core.usecase.profile import Profile
 
 app = Flask("posterr")
 
+
 @app.route("/")
 def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/profile/<username>", methods=["GET"])
+def profile(username):
+    profile = Profile(username)
+    return f"<p>Hello, {profile.payload()}!</p>"
+
+
+@app.route("/home", methods=["GET"])
+def home():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/post", methods=["GET", "POST"])
+def post():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/comment", methods=["GET", "POST"])
+def comment():
     return "<p>Hello, World!</p>"
