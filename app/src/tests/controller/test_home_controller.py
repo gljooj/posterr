6a94093ff98db4fdf03eea5d89e146d7a9a5f3b9
unknown <gabrielthe13@hotmail.com>
2022-07-20 00:30:18 -0300

    Tests home controller

--- app/src/tests/controller/test_home_controller.py
+++ app/src/tests/controller/test_home_controller.py
@@ -4,11 +4,22 @@ from src.controller.home_controller import HomeController
 
 
 class TestHomeController:
-    controller_success = HomeController(user="{'username': 'test'}", page=1,
+    controller_success = HomeController(user='{"username": "test"}', page=1,
                                         post_from='only-mine', start_at=None, end_at=None)
 
     def test_define_query_success(self):
-        assert self.controller_success.define_query() == "{'username': 'test'}"
+        assert self.controller_success.define_query() == self.controller_success.user
+        alternate_controller = self.controller_success
+        alternate_controller.post_from = "all"
+        assert alternate_controller.define_query() == {}
+
+    def test_define_query_error(self):
+        alternate_controller = self.controller_success
+        alternate_controller.post_from = "not_exist"
+        try:
+            self.controller_success.define_query()
+        except Exception as e:
+            assert e.args[0] == 'Filter must be all or only-mine'
 
     def test_define_date_query_success(self):
         controller_date = self.controller_success
@@ -27,3 +38,8 @@ class TestHomeController:
         controller_date.end_at = None
         assert controller_date.define_date_query() == {'created_date': {'$gte': datetime(2022, 7, 18, 4, 55, 5)}}
 
+    def test_define_date_query_none(self):
+        controller_date = self.controller_success
+        controller_date.start_at = None
+        controller_date.end_at = None
+        assert controller_date.define_date_query() is None
