9dbdb378394cc90010a2235f5865044a86eaf268
Gabriel Jesus <gabrielthe13@hotmail.com>
2022-07-21 13:27:56 -0300

    Readme and tutorial

---
+++ README.md
@@ -0,0 +1,122 @@
+# Posterr
+
+Mad with Flask, MongoDB and Docker. Dockerfile is also available to make docker image and docker containers.
+
+My name is Gabriel, So I'll explain how I thought to build this project:
+```
+       On the first step I was thinking something to have good Performance and be Scalable 
+       and "What the data the project will need and how I will have the control ?"
+    So, with these thinking I Decided to use a NoSql Database even the data talk each other, I think could be 
+    a good Ideia because, if in a near future need implements something new will not be a problem to new changes 
+    considering the quantity of users and posts.
+    And thinking this, below is the DataBase Modeling
+```
+## Database Modeling
+![Screenshot](posterr_database.jpg)
+
+```
+After create Database Modeling.
+        Now it's time to decide the architecture. I think this is one of the hard choices to do, because this decision will
+    reflect also the performance, easy understanding and scalability of the project. 
+    So I decided use concepts of Clean Architecture
+```
+
+
+## Built using :
+```sh
+	Flask : Python Based mini-Webframework
+	MongoDB : Database Server
+	Pymongo : Database Connector ( For creating connectiong between MongoDB and Flask )
+```
+
+## Set up environment for using this repo (ubuntu, Mac, Win):
+```
+Install Python ( If you don't have already )
+	$ sudo apt-get install python
+Install MongoDB ( Make sure you install it properly )
+	$ sudo apt install -y mongodb
+
+Windows:
+    Install Python 3.9 from https://www.python.org/downloads/
+
+    Install MongoDB from https://docs.docker.com/desktop/install/windows-install/
+
+    Install MongoDB from https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-windows/
+    
+    
+In Windows use Git bash: https://git-scm.com/downloads
+Install Dependencies of the application
+    in root project:
+    $ py -m pip install virtualenv
+    $ py -m venv venv
+    
+    Active venv
+    windows: source venv/Scripts/activate
+    linux: source venv/bin/activate
+    
+    Then:
+    $ pip install -r app/requirements.txt
+```
+
+## Run the application
+```
+On Linux/Mac (in windows if you follow the tutorial in mongo website, will start when turn on your computer):
+Start MongoDB
+	$ sudo service mongod start
+Start Docker
+    $ sudo service docker start    
+
+Start Application:
+    $ docker-compose build
+    $ docker-compose up
+    if you don't want to see the logs run ( docker-compose up -d)
+Stop Application:
+    $ CTRL + C or docker-compose down
+```
+
+## Run the Unnit tests
+
+```
+After Start Application:
+    $ docker exec -it posterr_web pytest
+```
+
+## Makefile
+
+```
+If you prefere you can prepare environment and use project by make commands:
+Commands
+    $ make setup
+    $ make build
+    $ make up
+    $ make down
+```
+
+## Collection Postman
+```
+The file Posterr.postman_collection.json is a postman collection
+Import the file in your postman for real tests
+
+Important: For all get requisitions we pass the user on header
+Folder - Profile
+            - Get Profile you can paginate(See the params)
+            - Create Profile
+            - Create Post From Profile
+       - Home
+            - Home (params to page)
+            - Home Only-Mine(with params to only mine)(See the params)
+            - Home with date_range [Dates must be Like '2022-07-17 00:00:00' or '2022-07-17'] (See the params)
+            - Create Post From Home
+       - Post
+            - Create post
+            - Create repost
+            - Create quote-post
+           
+ 
+Dates must be Like '2022-07-17 00:00:00' or '2022-07-17'
+
+If page not informed we pass the lastest 10
+
+About the scrolling HomePage/Profile. I did thinking on in front-end requests, 
+whenever the user reaches the bottom of page so it pass the param page.
+```
--- app/data_test_migrate.py
+++ app/data_test_migrate.py
@@ -11,75 +11,74 @@ class CreateTests(DataBaseConfig):
 
         if not self.__db.user.find_one({"username": "usertest1"}):
             self.create_user(self.__db.user)
-            self.create_post(self.__db.post)
             print("Success on Create data for tests")
             return "Success on Create data for tests"
 
         print("Data Already Created data for tests")
         return "Data Already Created data for tests"
 
-    @staticmethod
-    def create_user(user_collection):
-        number = 0
-        while number < 3:
+    def create_user(self, user_collection):
+        number = 1
+        while number <= 3:
             user_collection.insert_one({"username": f"usertest{number}",
                                         "joined_at": datetime(2022, 7, 17, 4 + number, 55, 5)})
-            number += 1
+            self.create_post(f"usertest{number}")
+            print(number)
+            number = number + 1
 
-    @staticmethod
-    def create_post(post_collection):
-        number_user = 1
+    def create_post(self, user):
+        post_collection = self.__db.post
         days_post = 1
-        post = object
-        while number_user < 3:
-            while days_post < 3:
-                post = post_collection.insert_many([{"type": "post", "username": f"usertest{number_user}",
-                                                     "text": "this is my post",
-                                                     "created_date": datetime(2022, 7, 18 + days_post, 4, 55, 5)},
+        post = []
+        while days_post <= 3:
+            post.append(post_collection.insert_many([{"type": "post", "username": f"{user}",
+                                                      "text": "this is my post",
+                                                      "created_date": datetime(2022, 7, 18 + days_post, 4, 55, 5)},
 
-                                                    {"type": "repost", "username": f"usertest{number_user}",
-                                                     "original_post": {"type": "post",
-                                                                       "username": f"usertest{number_user}",
-                                                                       "text": "this is my post"},
-                                                     "created_date": datetime(2022, 7, 18 + days_post, 5, 55, 5)},
+                                                     {"type": "repost", "username": f"{user}",
+                                                      "original_post": {"type": "post",
+                                                                        "username": f"{user}",
+                                                                        "text": "this is my post"},
+                                                      "created_date": datetime(2022, 7, 18 + days_post, 5, 55, 5)},
 
-                                                    {"type": "quote-post", "username": f"usertest{number_user}",
-                                                     "text": "This is my quote-post",
-                                                     "original_post": {"type": "post",
-                                                                       "username": f"usertest{number_user}",
-                                                                       "text": "this is my post",
-                                                                       "created_date": datetime(2022, 7, 18, 4, 55, 5)},
-                                                     "created_date": datetime(2022, 7, 18 + days_post, 6, 55, 5)},
+                                                     {"type": "quote-post", "username": f"{user}",
+                                                      "text": "This is my quote-post",
+                                                      "original_post": {"type": "post",
+                                                                        "username": f"{user}",
+                                                                        "text": "this is my post",
+                                                                        "created_date": datetime(2022, 7, 18, 4, 55,
+                                                                                                 5)},
+                                                      "created_date": datetime(2022, 7, 18 + days_post, 6, 55, 5)},
 
-                                                    {"type": "quote-post", "username": f"usertest{number_user}",
-                                                     "text": "This is my quote-post",
-                                                     "original_post": {"type": "repost",
-                                                                       "username": f"usertest{number_user}",
-                                                                       "original_post": {
-                                                                           "type": "post",
-                                                                           "username": f"usertest{number_user}",
-                                                                           "text": "this is my post"},
-                                                                       "created_date": datetime(2022, 7, 18, 5, 55, 5)},
-                                                     "created_date": datetime(2022, 7, 18 + days_post, 7, 55, 5)},
+                                                     {"type": "quote-post", "username": f"{user}",
+                                                      "text": "This is my quote-post",
+                                                      "original_post": {"type": "repost",
+                                                                        "username": f"{user}",
+                                                                        "original_post": {
+                                                                            "type": "post",
+                                                                            "username": f"{user}",
+                                                                            "text": "this is my post"},
+                                                                        "created_date": datetime(2022, 7, 18, 5, 55,
+                                                                                                 5)},
+                                                      "created_date": datetime(2022, 7, 18 + days_post, 7, 55, 5)},
 
-                                                    {"type": "repost", "username": f"usertest{number_user}",
-                                                     "original_post": {"type": "quote-post",
-                                                                       "username": f"usertest{number_user}",
-                                                                       "text": "This is my quote-post",
-                                                                       "original_post": {
-                                                                           "type": "post",
-                                                                           "username": f"usertest{number_user}",
-                                                                           "text": "this is my post",
-                                                                           "created_date": datetime(2022,
-                                                                                                    7, 18,
-                                                                                                    4, 55,
-                                                                                                    5)},
-                                                                       "created_date": datetime(2022, 7, 18, 6, 55, 5)},
-                                                     "created_date": datetime(2022, 7, 18 + days_post, 8, 55, 5)},
-
-                                                    ])
-                days_post += 1
-            number_user += 1
+                                                     {"type": "repost", "username": f"{user}",
+                                                      "original_post": {"type": "quote-post",
+                                                                        "username": f"{user}",
+                                                                        "text": "This is my quote-post",
+                                                                        "original_post": {
+                                                                            "type": "post",
+                                                                            "username": f"{user}",
+                                                                            "text": "this is my post",
+                                                                            "created_date": datetime(2022,
+                                                                                                     7, 18,
+                                                                                                     4, 55,
+                                                                                                     5)},
+                                                                        "created_date": datetime(2022, 7, 18, 6, 55,
+                                                                                                 5)},
+                                                      "created_date": datetime(2022, 7, 18 + days_post, 8, 55, 5)}
+                                                     ]).inserted_ids)
+            days_post = days_post + 1
 
         return post
 
